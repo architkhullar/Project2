840d6038f6155f13a915078fd3cd21aa29fcbfb7
Archit Khullar <akhullar@uncc.edu>
2018-09-14 22:03:31 -0400

    modifying the survey submit API with jwt and updating the postman json

--- API/api/controllers/SurveyController.js
+++ API/api/controllers/SurveyController.js
@@ -4,17 +4,25 @@ var mongoose = require('mongoose'),
   jwt = require('jsonwebtoken'),
   survey = mongoose.model('Survey');
 
-  exports.submit = function(req, res) {
+exports.submit = function(req, res) {
+  var newUsername = jwt.decode(req.headers.authorization);
+  var string = JSON.stringify(newUsername);
+  var objectValue = JSON.parse(string);
+  var getpatient = objectValue['username'];
+
   var newEntry = new Survey(req.body);
   newEntry.score = parseInt(req.body.q1) + parseInt(req.body.q2) + parseInt(req.body.q3) + parseInt(req.body.q4) + parseInt(req.body.q5) + parseInt(req.body.q6) + parseInt(req.body.q7) + parseInt(req.body.q8) + parseInt(req.body.q9) + parseInt(req.body.q10);
+  newEntry.PatientID = getpatient
   newEntry.save(function(err, survey) {
     if (err) {
       return res.status(400).send({
         message: err, status:'400'
       });
     } else {
-      //return res.json(newEntry);
       return res.json({score: newEntry.score, message: 'survey submitted successfully', status:'200'});
       }
   });
 };
+
+
+//exports.display_all_patients =

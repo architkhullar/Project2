26437e15a7753ec74617da464f26037d8b592fd4
Archit Khullar <akhullar@uncc.edu>
2018-09-17 20:48:10 -0400

    adding jwt admin for creatig the new patient account and updating POSTMAN file

--- API/api/controllers/PatientController.js
+++ API/api/controllers/PatientController.js
@@ -5,22 +5,39 @@ var mongoose = require('mongoose'),
   patient = mongoose.model('Patient');
 
   exports.register_patient = function(req, res) {
-  Patient.findOne({
-    username: req.body.username
-  }, function(err, patient) {
-    if (err) throw err;
-    if (!patient){
-      var newPatient = new Patient(req.body);
-      newPatient.save(function(err, patient) {
-        if (err) {
-          return res.status(400).send({message: err, status:'400'});
-        } else {
-          return res.json({username: newPatient.username , message: 'Patient Created', status:'200'});
-        }
-      });
-    } else {
-      return res.json({message: "this username already exists, select sowmthing else", status:'400'});
-    }
+    var newUsername = jwt.decode(req.headers.authorization);
+    console.log(newUsername);
+    var string = JSON.stringify(newUsername);
+    var objectValue = JSON.parse(string);
+    var getuser = objectValue['username'];
+
+    Admin.findOne({
+      username: getuser
+    }, function(err, user){
+      if (err) throw err;
+      if (!user) {
+        console.log('2');
+        res.status(401).json({ message: 'you do not have the admin priviliges', status: '401'});
+      } else if (user) {
+        Patient.findOne({
+          username: req.body.username
+        }, function(err, patient) {
+          if (err) throw err;
+          if (!patient){
+            var newPatient = new Patient(req.body);
+            newPatient.save(function(err, patient) {
+              if (err) {
+                return res.status(400).send({message: err, status:'400'});
+              } else {
+                return res.json({username: newPatient.username , message: 'Patient Created', status:'200'});
+              }
+            });
+          } else {
+            return res.json({message: "this username already exists, select sowmthing else", status:'400'});
+          }
+        });
+
+          }
   });
 };
 
@@ -49,7 +66,7 @@ exports.all_patient_data = function(req, res) {
     username: getuser
   }, function(err, user){
     if (err) throw err;
-    if (!admin) {
+    if (!user) {
       console.log('2');
       res.status(401).json({ message: 'you do not have the admin priviliges', status: '401'});
     } else if (user) {
